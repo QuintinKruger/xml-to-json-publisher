c9e35fbcb870ecac45b726c557dab9b05040bf16
QuintinKruger <krugerquintin5@gmail.com>
2026-07-30 08:05:31 +0200

    Init commit - setup new spring boot 3.4.1 application and create a hello world get endpoint

---
+++ src/main/java/org/example/xmltojsonpublisher/XmlToJsonPublisherApplication.java
@@ -0,0 +1,13 @@
+package org.example.xmltojsonpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToJsonPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlToJsonPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/xmltojsonpublisher/XmlToJsonPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xmltojsonpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlToJsonPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
