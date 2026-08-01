90ac3f9249b49272978b75dbcb5aefc2e11d20c5
QuintinKruger <krugerquintin5@gmail.com>
2026-08-01 13:39:29 +0200

    Move xml processing to service class

---
+++ src/main/java/org/example/xmltojsonpublisher/service/XmlProcessorService.java
@@ -0,0 +1,31 @@
+package org.example.xmltojsonpublisher.service;
+
+import org.example.xmltojsonpublisher.core.saver.Saver;
+import org.example.xmltojsonpublisher.core.transformer.XmlTransformer;
+import org.example.xmltojsonpublisher.core.transformer.rag.RagTransformer;
+import org.example.xmltojsonpublisher.domain.NormalizedJudgment;
+import org.springframework.stereotype.Service;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.InputStream;
+
+@Service
+public class XmlProcessorService {
+
+    private final XmlTransformer xmlTransformer;
+    private final RagTransformer ragTransformer;
+    private final Saver saver;
+
+    public XmlProcessorService(XmlTransformer xmlTransformer, RagTransformer ragTransformer, Saver saver) {
+        this.xmlTransformer = xmlTransformer;
+        this.saver = saver;
+        this.ragTransformer = ragTransformer;
+    }
+
+    public void processXmlFile(InputStream inputStream) throws Exception {
+        NormalizedJudgment normalizedJudgment = xmlTransformer.transform(new StreamSource(inputStream));
+        String ragText = ragTransformer.transform(normalizedJudgment);
+        saver.saveNormalizedJudgement(normalizedJudgment, normalizedJudgment.contentId());
+        saver.saveRagText(ragText, normalizedJudgment.contentId());
+    }
+}
--- src/main/java/org/example/xmltojsonpublisher/web/XmlToJsonPublisherController.java
+++ src/main/java/org/example/xmltojsonpublisher/web/XmlToJsonPublisherController.java
@@ -4,6 +4,7 @@ import org.example.xmltojsonpublisher.core.saver.Saver;
 import org.example.xmltojsonpublisher.core.transformer.XmlTransformer;
 import org.example.xmltojsonpublisher.core.transformer.rag.RagTransformer;
 import org.example.xmltojsonpublisher.domain.NormalizedJudgment;
+import org.example.xmltojsonpublisher.service.XmlProcessorService;
 import org.example.xmltojsonpublisher.validation.XmlValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,19 +21,15 @@ import java.util.List;
 public class XmlToJsonPublisherController {
 
     private final XmlValidator xmlValidator;
-    private final XmlTransformer xmlTransformer;
-    private final RagTransformer ragTransformer;
-    private final Saver saver;
+    private final XmlProcessorService xmlProcessorService;
 
 
     private record FileOutcome(String fileName, boolean processedSuccessfully, String exceptionMessage) {
     }
 
-    public XmlToJsonPublisherController(XmlValidator xmlValidator, XmlTransformer xmlTransformer, RagTransformer ragTransformer, Saver saver) {
+    public XmlToJsonPublisherController(XmlValidator xmlValidator, XmlProcessorService xmlProcessorService) {
         this.xmlValidator = xmlValidator;
-        this.xmlTransformer = xmlTransformer;
-        this.saver = saver;
-        this.ragTransformer = ragTransformer;
+        this.xmlProcessorService = xmlProcessorService;
     }
 
     @PostMapping(value = "/upload-xml", consumes = "multipart/form-data")
@@ -41,10 +38,7 @@ public class XmlToJsonPublisherController {
         for (MultipartFile multipartFile : multipartFiles) {
             try {
                 xmlValidator.validate(multipartFile);
-                NormalizedJudgment normalizedJudgment = xmlTransformer.transform(new StreamSource(multipartFile.getInputStream()));
-                String ragText = ragTransformer.transform(normalizedJudgment);
-                saver.saveNormalizedJudgement(normalizedJudgment, normalizedJudgment.contentId());
-                saver.saveRagText(ragText, normalizedJudgment.contentId());
+                xmlProcessorService.processXmlFile(multipartFile.getInputStream());
                 fileOutcomes.add(new FileOutcome(multipartFile.getOriginalFilename(), true, null));
             }  catch (Exception e) {
                 fileOutcomes.add(new FileOutcome(multipartFile.getOriginalFilename(), false, "%s - %s".formatted(e.getClass().getSimpleName(), e.getMessage())));
